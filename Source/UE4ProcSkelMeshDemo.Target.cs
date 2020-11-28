17d8d0d71654bf007c685418f1d2140a7b9d8d2c
monguri <mongry@gmail.com>
2020-11-28 16:45:22 +0900

    Blankテンプレートからプロジェクトを作成した

---
+++ Source/UE4ProcSkelMeshDemo.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class UE4ProcSkelMeshDemoTarget : TargetRules
+{
+	public UE4ProcSkelMeshDemoTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "UE4ProcSkelMeshDemo" } );
+	}
+}
---
+++ Source/UE4ProcSkelMeshDemo/UE4ProcSkelMeshDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4ProcSkelMeshDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4PROCSKELMESHDEMO_API AUE4ProcSkelMeshDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
